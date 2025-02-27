90c3ecf993d16c4b9bf4b18d7490124a857ecb82
datkoi211 <truongdinhdetrel@gmail.com>
2025-02-28 05:36:09 +0700

    Initial commit

---
+++ tests/unit/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    mocha: true
+  }
+}
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended"
+  ],
+  parserOptions: {
+    ecmaVersion: 2021
+  },
+  rules: {}
+};
